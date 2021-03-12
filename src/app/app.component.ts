dbe47b45fa756ba6ab18330ed19d74b73a8e3eee
SZamboni <stefano.zamboni@intesa.it>
2021-03-12 16:31:37 +0100

    "first page bozza"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <!-- header -->
+    <app-header></app-header>
+
+
+    <!-- routes get injected here -->
+    <router-outlet></router-outlet>
+
+    <!-- footer -->
+    <app-footer></app-footer>
+
+  `,
+  styles: []
 })
 export class AppComponent {
   title = 'personal-website';
